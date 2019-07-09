cd19b049db07d857c5cd7c943405528cf6efaad3
Yaroslav Onuchak <51050378+YaroslavOnuchak@users.noreply.github.com>
2019-07-09 23:57:37 +0300

    Add files via upload

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,31 +1,29 @@
-let num, numPocket, ask, increase = 4, userPrize = 0, minDefNum = 0, maxDefNum = 8;
-let firstAt = 100, secondAt = 50, thirdAt = 25;
-let prize = [firstAt, secondAt, thirdAt];
+let number, start, numPocket, increase = 4, userPrize = 0, minDefNum = 0, maxDefNum = 8;
+let atteFirst = 100, atteSecon = 50, atteThir = 25;
+let prize = [atteFirst, atteSecon, atteThir];
 let maxNum = maxDefNum, minNum = minDefNum;
-
-
 do {
-    ask = confirm(`Do you want to play a game?`);
-    if (!ask) {
+    start = confirm(`Do you want to play a game?`);
+    if (!start) {
         alert(`You did not become a billionaire, but can.`);
     }
-} while (!ask);
+} while (!start);
 do {
     let attempts = 2, win = false, attemptsLeft = attempts + 1;
-    num = Math.round(Math.random() * (maxNum - minNum) + minNum);
+    number = Math.floor(Math.random() * (maxNum - minNum) + minNum);
     while (win === false) {
         for (let i = 0; i <= attempts; i++) {
-            let messageMinMax = `Choose the roulette pocket number from ${minNum} to ${maxNum}\n`;
+            let messageMinMax = `Choose a roulette pocket number from ${minNum} to ${maxNum}\n`;
             let messageAttempts = `Attempts left: ${attemptsLeft}\n`;
             let totalPrize = `Total prize: ${userPrize}\n`
             let posiblePrize = `Possible price on current attempt: ${prize[i]}`;
             let message = `${messageMinMax}${messageAttempts}${totalPrize}${posiblePrize}`;
             numPocket = +prompt(`Enter a number of pocket on which the ball could land. \n${message}`);
             console.log(numPocket);
-            if (num === numPocket) {
+            if (number === numPocket) {
                 userPrize += prize[i];
-                ask = confirm(`Congratulation, you won! Your prize is: ${userPrize} $.\nDo you want to continue?`);
-                if (ask) {
+                start = confirm(`Congratulation, you won! Your prize is: ${userPrize} $.\nDo you want to continue?`);
+                if (start) {
                     win = true;
                     i = attempts;
                     maxNum += increase;
@@ -38,13 +36,13 @@ do {
             } else {
                 alert(`Thank you for your participation. Your prize is: ${userPrize} $`);
                 if (i === attempts) {
-                    ask = confirm(`Do you want to play a game again?`);
+                    start = confirm(`Do you want to play a game again?`);
                     win = true;
                     userPrize = 0;
                     maxNum = maxDefNum;
-                    prize = [firstAt, secondAt, thirdAt];
+                    prize = [atteFirst, atteSecon, atteThir];
                 }
             } attemptsLeft--;
         }
     }
-} while (ask);
+} while (start);
