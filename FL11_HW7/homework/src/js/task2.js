448c028b5e227605e1ae22be2a7e6e8c2f48e4df
Yaroslav Onuchak <51050378+YaroslavOnuchak@users.noreply.github.com>
2019-07-10 00:05:49 +0300

    Add files via upload

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,7 +1,9 @@
-let number, start, numPocket, increase = 4, userPrize = 0, minDefNum = 0, maxDefNum = 8;
+let number, start, increase = 4, userPrize = 0, numPock, minDefNum = 0, maxDefNum = 8;
 let atteFirst = 100, atteSecon = 50, atteThir = 25;
 let prize = [atteFirst, atteSecon, atteThir];
-let maxNum = maxDefNum, minNum = minDefNum;
+let numMax = maxDefNum;
+let numMin = minDefNum;
+
 do {
     start = confirm(`Do you want to play a game?`);
     if (!start) {
@@ -10,23 +12,23 @@ do {
 } while (!start);
 do {
     let attempts = 2, win = false, attemptsLeft = attempts + 1;
-    number = Math.floor(Math.random() * (maxNum - minNum) + minNum);
+    number = Math.floor(Math.random() * (numMax - numMin) + numMin);
     while (win === false) {
         for (let i = 0; i <= attempts; i++) {
-            let MinMax = `Choose a roulette pocket number from ${minNum} to ${maxNum}\n`;
+            let MinMax = `Choose a roulette pocket number from ${numMin} to ${numMax}\n`;
             let Attempt = `Attempts left: ${attemptsLeft}\n`;
             let totalPrize = `Total prize: ${userPrize}\n`
             let posiblePrize = `Possible price on current attempt: ${prize[i]}`;
             let message = `${MinMax}${Attempt}${totalPrize}${posiblePrize}`;
-            numPocket = +prompt(`Enter a number of pocket on which the ball could land. \n${message}`);
+            numPock = +prompt(`Enter a number of pocket on which the ball could land. \n${message}`);
             console.log(numPocket);
-            if (number === numPocket) {
+            if (number === numPock) {
                 userPrize += prize[i];
                 start = confirm(`Congratulation, you won! Your prize is: ${userPrize} $.\nDo you want to continue?`);
                 if (start) {
                     win = true;
                     i = attempts;
-                    maxNum += increase;
+                    numMax += increase;
                     for (let j = 0; j < prize.length; j++) {
                         prize[j] *= 2;
                     }
@@ -39,7 +41,7 @@ do {
                     start = confirm(`Do you want to play a game again?`);
                     win = true;
                     userPrize = 0;
-                    maxNum = maxDefNum;
+                    numMax = maxDefNum;
                     prize = [atteFirst, atteSecon, atteThir];
                 }
             } attemptsLeft--;
