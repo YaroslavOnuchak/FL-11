fd1ef1b416baadb4166663474c7378a121967da1
Yaroslav Onuchak <51050378+YaroslavOnuchak@users.noreply.github.com>
2019-07-24 05:16:52 +0300

    Delete app.js

--- FL11_HW11/homework/FL11_HW12/homework/src/app.js
+++
@@ -1,9 +0,0 @@
-const rootNode = document.getElementById('root');
-
-const todoItems = [
-    {isDone: false, id: 12345, description: 'Todo 1'}
-];
-
-// Your code goes here
-
-rootNode.appendChild(/* Append your list item node*/);
